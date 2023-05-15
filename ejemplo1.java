bfe45bf8c8b3860df85d81f05ab9a265eff0334c
Ruben Marin Silva <ercaricaceres@gmail.com>
2023-05-15 09:44:41 +0000

    primer commit lunes

--- ejemplo1.java
+++ ejemplo1.java
@@ -5,7 +5,6 @@ public class ejemplo1{
         for(int i = 0; i <= 12; i++)
         {
             System.out.print("12 * "+ i + " = " + 12 * i + "\n");
-            System.out.print("12 * "+ i*2 + " = " + 12 * i + "\n");
 
         }
     }
