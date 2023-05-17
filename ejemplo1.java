854cc29f5ad30a203f6ea72585f9bc25c3e4b06e
juancasanchez6 <131864347+juancasanchez6@users.noreply.github.com>
2023-05-17 11:05:55 +0200

    Update ejemplo1.java

--- ejemplo1.java
+++ ejemplo1.java
@@ -7,7 +7,11 @@ public class ejemplo1{
             System.out.println("Hola, mi nombre es Javier");
 
             System.out.print("12 * "+ i + " = " + 12 * i + "\n");
-            //Juan Carlos Sanchez
+            
+            
+            
+            System.out.println ("Hola me llamo: ");
+            System.out.println ("JUan CArlos");
 
             System.out.println("Hola me llamo :");
             System.out.println("David");
