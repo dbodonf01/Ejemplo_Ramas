aa5e5073367dcad1ef218d37bb8b33d8b0206371
dbodonf01 <131864556+dbodonf01@users.noreply.github.com>
2023-05-17 10:57:18 +0200

    Update ejemplo1.java

--- ejemplo1.java
+++ ejemplo1.java
@@ -4,7 +4,7 @@ public class ejemplo1{
     {
         for(int i = 0; i <= 12; i++)
         {
-            System.out.print("12 * "+ i + " = " + 12 * i + "\n");
+            System.out.print("Hola me llamo David");
 
         }
     }
